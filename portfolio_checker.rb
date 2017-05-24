6659148f5f12fdcfeb19fdc805c1f261ff08d6d9
Ben Plunkert <ben.plunkert@gmail.com>
2017-05-23 23:58:02 -0400

    add btc balances

--- portfolio_checker.rb
+++ portfolio_checker.rb
@@ -14,11 +14,14 @@ usd_prices = {
   'XMR'  => JSON.parse(HTTParty.get('https://api.cryptowat.ch/markets/kraken/xmrusd/price').to_s)['result']['price']
 }
 
-usd_balances = balances.collect{|coin, coin_balance| [coin, (coin_balance * usd_prices[coin])]}.to_h
-total_balance = usd_balances.values.reduce(0, :+)
+usd_balances = balances.map{|coin, coin_balance| [coin, (coin_balance * usd_prices[coin])]}.to_h
+btc_balances = balances.map{|coin, coin_balance| [coin, (coin_balance * usd_prices[coin] / usd_prices['BTC'])]}.to_h
+total_usd_balance = usd_balances.values.reduce(0, :+)
+total_btc_balance = btc_balances.values.reduce(0, :+)
 
 usd_balances.each do |coin, balance|
-  puts "#{coin}: $#{sprintf("%.2f", balance)} / #{sprintf("%.2f", 100*balance/total_balance)}%" if balance != 0
+  puts "#{coin}: $#{sprintf("%.2f", balance)} / #{sprintf("%.2f", 100*balance/total_usd_balance)}% (#{ sprintf("%.8f",  btc_balances[coin])} BTC)" if balance != 0
 end
 
-puts "Total Portfolio Balance: #{sprintf("%.2f", total_balance)}"
+puts "Total Portfolio USD Balance: $#{sprintf("%.2f", total_usd_balance)} USD"
+puts "Total Portfolio BTC Balance: #{sprintf("%.8f", total_btc_balance)} BTC"
