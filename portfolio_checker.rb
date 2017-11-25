968b5fe79d88403fc651a6430b6d27e2e8d8ca36
Ben Plunkert <ben.plunkert@gmail.com>
2017-11-25 18:48:19 -0500

    add bitcoin cash total balances feature

--- portfolio_checker.rb
+++ portfolio_checker.rb
@@ -28,14 +28,14 @@ usd_prices = {
 
 usd_balances = balances.map{|coin, coin_balance| [coin, (coin_balance * usd_prices[coin])]}.to_h
 btc_balances = balances.map{|coin, coin_balance| [coin, (coin_balance * usd_prices[coin] / usd_prices['BTC'])]}.to_h
+bch_balances = balances.map{|coin, coin_balance| [coin, (coin_balance * usd_prices[coin] / usd_prices['BCH'])]}.to_h
 total_usd_balance = usd_balances.values.reduce(0, :+)
 total_btc_balance = btc_balances.values.reduce(0, :+)
+total_bch_balance = bch_balances.values.reduce(0, :+)
 
 usd_balances.each do |coin, balance|
-  puts "#{coin}: $#{sprintf("%.2f", balance)} / #{sprintf("%.2f", 100*balance/total_usd_balance)}% (#{ sprintf("%.8f",  btc_balances[coin])} BTC)" if balance != 0
+  puts "#{coin}: $#{sprintf("%.2f", balance)} / #{sprintf("%.2f", 100*balance/total_usd_balance)}% (#{ sprintf("%.8f",  btc_balances[coin])} BTC  /  #{ sprintf("%.8f",  bch_balances[coin])} BCH)" if balance != 0
 end
 
-puts "Total Portfolio USD Balance: $#{sprintf("%.2f", total_usd_balance)} USD"
-puts "Total Portfolio BTC Balance: #{sprintf("%.8f", total_btc_balance)} BTC"
-
+puts "Total Portfolio Balance: $#{sprintf("%.2f", total_usd_balance)} USD  /  #{sprintf("%.8f", total_btc_balance)} BTC  /  #{sprintf("%.8f", total_bch_balance)} BCH"
 puts "BTC+BCH+BTG Combined Ticker Price = $#{sprintf("%.2f", usd_prices['BTC'] + usd_prices['BCH'] + usd_prices['BTG'])}"
